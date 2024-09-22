4c5b61d713dc181d01870d13145c68549d329a3c
Byron Machin <bmachin7@gmail.com>
2024-09-21 20:05:47 -0500

    urls para resolver las consultas a los productos.

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ProductListView, ProductFormView, ProductDetailView
+
+urlpatterns = [
+    path('product-list/', ProductListView.as_view(), name='product_list'),
+    path('create-product/', ProductFormView.as_view(), name='product_form'),
+    path('product-detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+]
