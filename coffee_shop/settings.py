dcf50d4a02edd0e3348044fcad2463956ab24c03
Byron Machin <bmachin7@gmail.com>
2024-09-22 11:01:08 -0500

    Configuraciones para manejo de redirecciones.

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+LOGIN_REDIRECT_URL = "product_list"
+LOGOUT_REDIRECT_URL = "logout"
