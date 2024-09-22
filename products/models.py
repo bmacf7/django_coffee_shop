fce1a3887a2b558d3bc7da447f6b5c853c423de4
Byron Machin <bmachin7@gmail.com>
2024-09-22 09:10:40 -0500

    Agregados campos para fecha de creacion y actualizacion.

--- products/models.py
+++ products/models.py
@@ -6,7 +6,15 @@ class Product(models.Model):
     description = models.TextField(max_length=200, verbose_name="Descripcion")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
     available = models.BooleanField(default=True, verbose_name="Disponibilidad")
-    image = models.ImageField(upload_to="products", null=True, blank=True, verbose_name="Foto")
+    image = models.ImageField(
+        upload_to="products", null=True, blank=True, verbose_name="Foto"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creacion"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualizacion"
+    )
 
     def __str__(self):
         return self.name
