e053149cec4af2def76ca4be12f97f4da521fcf0
Byron Machin <bmachin7@gmail.com>
2024-09-21 20:06:10 -0500

    Vistas que manejan los datos de los productos.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Product
+from .forms import ProductForm
+from django.views.generic import ListView, FormView, DetailView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+class ProductFormView(FormView):
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
