732b0dab23f46e3baf89b7f18b8def1116f8d45a
Byron Machin <bmachin7@gmail.com>
2024-09-22 11:01:58 -0500

    Clase para manejar los productos desde el django admin.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ("name", "price", "created_at", "available")
+    search_fields = ("name", "description")
+
+
+admin.site.register(Product, ProductAdmin)
